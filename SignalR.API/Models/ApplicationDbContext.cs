3bff49a82caf93ac4de6eeab8c50469791272226
muratilgun <muratilgun34@gmail.com>
2021-10-31 00:46:21 +0300

    Client.Group

---
+++ SignalR.API/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+        }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SignalR.API/Models/Team.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Models
+{
+    public class Team
+    {
+        
+        public Team()
+        {
+            Users = new List<User>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Bunun virtual yapılma sebebi Lazy Loading desteği
+        // Ayrıca entity üzerindeki değişiklikleri yakalayabilsin diye
+        // ICollection ile beraber gelen Add Contains gibi metotlarını
+        // kullanmak için ICollection tanımlandı.
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
