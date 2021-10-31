ae76bc242d3e5428157226bf78c7e35098fe7a2b
muratilgun <muratilgun34@gmail.com>
2021-10-31 22:24:42 +0300

    Added covidservice

---
+++ CovidChart.API/Models/CovidService.cs
@@ -0,0 +1,34 @@
+﻿using CovidChart.API.Hubs;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidChart.API.Models
+{
+    public class CovidService
+    {
+        private readonly AppDbContext _context;
+        private readonly IHubContext<CovidHub> _hubContext;
+        public CovidService(AppDbContext context, IHubContext<CovidHub> hubContext)
+        {
+            _context = context;
+            _hubContext = hubContext;
+        }
+        public IQueryable<Covid> GetList()
+        {
+            return _context.Covids.AsQueryable();
+            //Ienumarable tüm database e sorgu atar daha sonra filtre işlemi için
+            // ikinci sorgu gönderir. IQueryable ise direk query gönderdiği için tek sorguda
+            // işlemi tamamlar.
+        }
+
+        public async Task SaveCovid(Covid covid)
+        {
+            await _context.Covids.AddAsync(covid);
+            await _context.SaveChangesAsync();
+            await _hubContext.Clients.All.SendAsync("ReceiveCovidList", "");
+        }
+    }
+}
