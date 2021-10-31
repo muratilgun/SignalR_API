a1ea50beb26743fefa1006f07f8c20aeb96273ac
muratilgun <muratilgun34@gmail.com>
2021-10-31 16:08:26 +0300

    Complex Type

--- SignalR.API/Hubs/MyHub.cs
+++ SignalR.API/Hubs/MyHub.cs
@@ -18,6 +18,10 @@ namespace SignalR.API.Hubs
         private static List<string> Names { get; set; } = new List<string>();
         private static int ClientCount { get; set; } = 0;
         public static int TeamCount { get; set; } = 7;
+        public async Task SendProduct(Product p)
+        {
+            await Clients.All.SendAsync("ReceiveProduct",p);
+        }
         public async Task SendName(string name)
         {
             if (Names.Count >= TeamCount)
